a801abe6be424ec546ecc537440db217e7d76da0
armandoahir <armandoahir31@gmail.com>
2026-05-03 11:22:09 +0200

    [3rd May] Books section update

---
+++ assets/js/books.js
@@ -0,0 +1,18 @@
+// campo 'slug' corrispondeval permalink del file markdown 
+//nella cartella _book-reviews/
+export const bookEntries = [
+  {
+    slug: "the-little-prince",
+    title: "The Little Prince",
+    author: "Antoine de Saint-Exupéry",
+    tag: "Novella",
+    year: 2026,
+  },
+  {
+    slug: "restaurant-at-the-end-of-the-universe",
+    title: "Restaurant At The End Of The Universe",
+    author: "Douglas Adams",
+    tag: "Sci-fi",
+    year: 2026,
+  },
+];
---
+++ the-little-prince.md
@@ -0,0 +1,16 @@
+---
+layout: page
+title: "The Little Prince"
+author: ""
+tag: "Novella"
+year: 2026
+cover: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1332615001i/71091.jpg"
+permalink: /book-reviews/the-little-prince/
+---
+## My review
+
+in progress..
+
+---
+
+*Read in 2026.*
