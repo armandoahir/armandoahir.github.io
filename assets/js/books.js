d7a86af8e4341127ad3f744d0fb5f1c3a0cf627c
armandoahir <armandoahir@users.noreply.github.com>
2026-05-07 09:42:51 +0000

    deploy: 4e3de8eae711f02ce0630841d6c8d5bf0e88d2e6

---
+++ assets/js/books.js
@@ -0,0 +1,17 @@
+const bookEntries = [
+{ title: "White nights", author: "F. Dostoevsky", date: "May 2026"},
+{ title: "Animal farm", author: "G. Orwell", date: "May 2026" },
+{ title: "Restaurant at the end of the universe", author: "D. Adams", date: "May 2026" },
+{ title: "The little prince", author: "A. de Saint-Exupéry", date: "May 2026" },
+{ title: "The enchiridion", author: "Epictetus", date: "Feb 2026" },
+{ title: "Gratitude", author: "O. Sacks", date: "Feb 2021" },
+{ title: "The process", author: "F. Kafka", date: "Feb 2021" },
+{ title: "The Metamorfosis", author: "F. Kafka", date: "Jan 2021" },
+{ title: "Elon Musk: Tesla, SpaceX, and the quest for a fantastic future", author: "A. Vance", date: "Jan 2021" },
+{ title: "The hitchhiker's guide to the Galaxy", author: "D. Adams", date: "Dec 2020" },
+{ title: "Siddartha", author: "H. Hesse", date: "Mar 2018" },
+{ title: "Milk and honey", author: "R. Kaur", date: "Feb 2017" },
+{ title: "Rich dad poor dad", author: "R. Kyiosaki", date: "Aug 2016" }
+];
+
+window.bookEntries = bookEntries;
---
+++ assets/js/places.js
@@ -0,0 +1,14 @@
+const placeEntries = [
+    { country: "Italy",        year: "2005", comment: "",           			    commentId: "italy-desc",  commentLabel: "Click for more" },
+    { country: "India",        year: "2009", comment: "",            			    commentId: "india-desc",  commentLabel: "Click for more" },
+    { country: "Austria",      year: "2013", comment: "School trip.",                       commentId: "",            commentLabel: "" },
+    { country: "Germany",      year: "2013", comment: "Christmas 2013 trip.",               commentId: "",            commentLabel: "" },
+    { country: "France",       year: "2013", comment: "Christmas 2013 trip.",               commentId: "",            commentLabel: "" },
+    { country: "England",      year: "2015", comment: "London, Camden Town, Birmingham.",   commentId: "",            commentLabel: "" },
+    { country: "Scotland",     year: "2016", comment: "Glasgow.",                           commentId: "",            commentLabel: "" },
+    { country: "Vatican City", year: "2018", comment: "",                                   commentId: "",            commentLabel: "" },
+    { country: "Ireland",      year: "2019", comment: "Dublin, Google headoffice",          commentId: "",            commentLabel: "" },
+    { country: "Albania",      year: "2023", comment: "Weekend fastlife.",                  commentId: "",            commentLabel: "" },
+];
+
+window.placeEntries = placeEntries;
