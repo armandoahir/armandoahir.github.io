193924101e3a3b631bf95c8201d726693147de4f
Armando Ahir <arma@archserver.localdomain>
2026-05-28 09:35:50 +0200

    Fix typo

--- assets/js/books.js
+++ assets/js/books.js
@@ -8,7 +8,7 @@ const bookEntries = [
 { title: "The enchiridion", author: "Epictetus", date: "Feb 2026" },
 { title: "Gratitude", author: "O. Sacks", date: "Feb 2021" },
 { title: "The process", author: "F. Kafka", date: "Feb 2021" },
-{ title: "The Metamorfosis", author: "F. Kafka", date: "Jan 2021" },
+{ title: "The Metamorphosis", author: "F. Kafka", date: "Jan 2021" },
 { title: "Elon Musk: Tesla, SpaceX, and the quest for a fantastic future", author: "A. Vance", date: "Jan 2021" },
 { title: "The hitchhiker's guide to the Galaxy", author: "D. Adams", date: "Dec 2020" },
 { title: "Siddartha", author: "H. Hesse", date: "Mar 2018" },
