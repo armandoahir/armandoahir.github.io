a526f1fba9ff75efee9ea61efd80f6f762a22c68
armandoahir <armandoahir31@gmail.com>
2026-07-31 15:53:21 +0200

    bird update

--- assets/js/birds.js
+++ assets/js/birds.js
@@ -41,7 +41,8 @@ const birdEntries = [
   { common: "Great Reed Warbler", latin: "Acrocephalus arundinaceus", date: "19.04.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/45042681/320" },
   { common: "Western jackdown", latin: "Coloeus monedula", date: "19.04.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/611135886/320" },
   { common: "Great cormorant", latin: "Phalacrocorax carbo", date: "19.04.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/66026521/320" },
- { common: "Spotted flycatcher", latin: "Muscicapa striata", date: "23.05.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v2/asset/613402295/320" }
+  { common: "Spotted flycatcher", latin: "Muscicapa striata", date: "23.05.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v2/asset/613402295/320" },
+  { common: "Cirl bunting", latin: "Emberiza cirlus", date: "29.07.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/169219721/320" }
 ];
 
 const lastUpdated = birdEntries
