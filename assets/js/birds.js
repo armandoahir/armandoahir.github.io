d19599edd816b915b89b09f298589975d835154d
armandoahir <armandoahir31@gmail.com>
2026-08-30 12:19:11 +0200

    bird update: typo fix

--- assets/js/birds.js
+++ assets/js/birds.js
@@ -43,7 +43,7 @@ const birdEntries = [
   { common: "Great cormorant", latin: "Phalacrocorax carbo", date: "19.04.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/66026521/320" },
   { common: "Spotted flycatcher", latin: "Muscicapa striata", date: "23.05.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v2/asset/613402295/320" },
   { common: "Cirl bunting", latin: "Emberiza cirlus", date: "29.07.2026", img: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/169219721/320" },
-  { common: "Pygmy cormorant", latin: "Microcarbo pygmaeus", date: "30.0.2026", img:"https://cdn.download.ams.birds.cornell.edu/api/v1/asset/258089531/320"}, 
+  { common: "Pygmy cormorant", latin: "Microcarbo pygmaeus", date: "30.08.2026", img:"https://cdn.download.ams.birds.cornell.edu/api/v1/asset/258089531/320"}, 
   { common: "Eurasian jay" , latin: "Garrulus glandarius", date: "30.08.2026", img:"https://cdn.download.ams.birds.cornell.edu/api/v1/asset/252252921/320" }
 ];
 
